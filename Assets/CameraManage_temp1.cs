2de1a3b248d9ec573fb059e15443175b0e2b0441
Denis Nemchenko <danieldesua@gmail.com>
2023-10-12 11:14:29 +0300

    temp 1

---
+++ Assets/CameraManage_temp1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManage_temp1 : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraOne;
+    [SerializeField] private GameObject cameraTwo;
+    [SerializeField] private float delay = .5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SwitchCamera());
+    }
+
+    // Update is called once per frame
+    IEnumerator SwitchCamera()
+    {
+        while (true)
+        {
+            cameraOne.SetActive(false);
+            cameraTwo.SetActive(false);
+            yield return new WaitForSeconds(delay);
+            cameraOne.SetActive(true);
+            cameraTwo.SetActive(false);
+            yield return new WaitForSeconds(delay);
+            cameraOne.SetActive(false);
+            cameraTwo.SetActive(true);
+            yield return new WaitForSeconds(delay);
+            cameraOne.SetActive(true);
+            cameraTwo.SetActive(true);
+            yield return new WaitForSeconds(delay); 
+        }
+        
+    }
+}
